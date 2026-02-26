ea3ba260d1082fc80276c3dfa49373916c364a99
yudong27 <yuu1010@qq.com>
2026-02-26 14:57:44 +0800

    del python

--- run_github.sh
+++
@@ -1,4 +0,0 @@
-# Linux/Mac
-export PYTHONPATH=./tech_report
-# 然后可以任何地方运行
-python tech_reporter.py
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,26 @@
 import Container from "@/app/_components/container";
 import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
-import { MoreStories } from "@/app/_components/more-stories";
 import { getAllPosts } from "@/lib/api";
 
 export default function Index() {
   const allPosts = getAllPosts();
 
-  const heroPost = allPosts[0];
-
-  const morePosts = allPosts.slice(1);
-
   return (
     <main>
       <Container>
         <Intro />
-        <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        />
-        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        {allPosts.map((post) => (
+          <HeroPost
+            key={post.slug}
+            title={post.title}
+            coverImage={post.coverImage}
+            date={post.date}
+            author={post.author}
+            slug={post.slug}
+            excerpt={post.excerpt}
+          />
+        ))}
       </Container>
     </main>
   );
